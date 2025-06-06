c0521d9545d011d3eee3f8fcb179c696190373b5
timvisee <tim@visee.me>
2025-06-06 12:56:51 +0200

    Replace RocksDB sparse storage with Gristore in mutable text index tests

--- lib/segment/src/index/field_index/full_text_index/mutable_text_index.rs
+++ lib/segment/src/index/field_index/full_text_index/mutable_text_index.rs
@@ -301,7 +301,6 @@ mod tests {
     use tempfile::Builder;
 
     use super::*;
-    use crate::common::rocksdb_wrapper::open_db_with_existing_cf;
     use crate::data_types::index::{TextIndexType, TokenizerType};
     use crate::json_path::JsonPath;
     use crate::types::{FieldCondition, Match};
@@ -310,10 +309,8 @@ mod tests {
         FieldCondition::new_match(JsonPath::new("text"), Match::new_text(text))
     }
 
-    #[rstest]
-    #[case(true)]
-    #[case(false)]
-    fn test_full_text_indexing(#[case] immutable: bool) {
+    #[test]
+    fn test_full_text_indexing() {
         use common::counter::hardware_accumulator::HwMeasurementAcc;
         use common::counter::hardware_counter::HardwareCounterCell;
         use common::types::PointOffsetType;
@@ -349,11 +346,10 @@ mod tests {
         };
 
         {
-            let db = open_db_with_existing_cf(&temp_dir.path().join("test_db")).unwrap();
-
-            let mut index = FullTextIndex::builder_rocksdb(db, config.clone(), "text")
-                .make_empty()
-                .unwrap();
+            let mut index =
+                FullTextIndex::new_gridstore(temp_dir.path().join("test_db"), config).unwrap();
+            let loaded = index.load().unwrap();
+            assert!(loaded);
 
             let hw_cell = HardwareCounterCell::new();
 
@@ -420,8 +416,8 @@ mod tests {
         }
 
         {
-            let db = open_db_with_existing_cf(&temp_dir.path().join("test_db")).unwrap();
-            let mut index = FullTextIndex::new_rocksdb(db, config, "text", immutable);
+            let mut index =
+                FullTextIndex::new_gridstore(temp_dir.path().join("test_db"), config).unwrap();
             let loaded = index.load().unwrap();
             assert!(loaded);
 
